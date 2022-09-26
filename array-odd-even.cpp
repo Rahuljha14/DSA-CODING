69759bb7a18a44e777a74c908e3d6a71ac93665e
Rahuljha14 <102228344+Rahuljha14@users.noreply.github.com>
2022-09-26 16:44:55 +0530

    Add files via upload

---
+++ array-odd-even.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************
+ARRANGE All odd number first then even of the ARRAY
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int n;
+   cin>>n;
+   int arr[n];
+   
+   for(int i=0;i<n;i++)
+   {
+       cin>>arr[i];
+   }
+   for(int i=0;i<n;i++)
+   {
+       
+       if(arr[i]%2!=0)
+       cout<<arr[i];
+   }
+   for(int i=0;i<n;i++)
+   {
+       
+       if(arr[i]%2==0)
+       cout<<arr[i];
+   }
+  
+   
+
+    return 0;
+}
